ae9fd0c18cb9d8f6dd1c25260ae701d7f2dfac35
duvan <duvanfoz@hotmail.com>
2022-09-08 20:18:51 -0500

    feat: longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,53 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
+
+  def home
+  end
+
   def new
+    @letters = generate_grid(params[:size].to_i)
   end
 
   def score
+    @result = if word_exists?(params[:word]) && word_in_grid(params[:word], params[:odioso].chars)
+                "Congrulations! #{params[:word]} is a valid English Word!"
+              elsif !word_exists?(params[:word])
+                'Not an English Word!'
+              else
+                'Not in the grid!'
+              end
   end
+end
+
+def generate_grid(grid_size)
+  letters = ('A'..'Z').to_a
+  grid = []
+  count = 0
+  while count != grid_size
+    index = rand(0..25)
+    grid.push(letters[index])
+    count += 1
+  end
+  return grid
+end
+
+def word_exists?(word)
+  url = "https://wagon-dictionary.herokuapp.com/#{word}"
+  url_ = URI.open(url).read
+  json_response = JSON.parse(url_)
+  return json_response['found']
+end
+
+def word_in_grid(word, grid)
+  word.upcase.chars do |char|
+    if grid.include?(char)
+      index = grid.index(char)
+      grid.delete_at(index)
+    else
+      return false
+    end
+  end
+  return true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'games/new'
-  post 'games/score', to: 'games#score'
+  root 'games#home'
+  get 'new', to: 'games#new', as: 'new'
+  get 'score', to: 'games#score', as: 'score'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
